8c1ea01e3cd0be77333291244b4cbfde33a68d2b
crachetoni-at-152410181392 <crachetonireformam@gmail.com>
2025-02-23 18:03:45 -0300

    initial commit

---
+++ src/test/java/com/henry/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
