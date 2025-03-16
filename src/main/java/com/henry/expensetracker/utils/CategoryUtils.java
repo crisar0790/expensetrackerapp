0637b01edbc20b4e05879f2a36c1802aa81c18e5
crachetoni-at-152410181392 <crachetonireformam@gmail.com>
2025-03-16 15:21:16 -0300

    add utils

---
+++ src/main/java/com/henry/expensetracker/utils/CategoryUtils.java
@@ -0,0 +1,26 @@
+package com.henry.expensetracker.utils;
+
+import com.henry.expensetracker.controller.model.response.CategoryResponse;
+import com.henry.expensetracker.exception.GetCategoryException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryUtils {
+    private final String SQL_GET_CATEGORY_BY_NAME = "SELECT name FROM category WHERE name = ?";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Long getCategoryId(String name) throws GetCategoryException {
+        try {
+            return jdbcTemplate.queryForObject(SQL_GET_CATEGORY_BY_NAME, new Object[]{name}, Long.class);
+        } catch (EmptyResultDataAccessException e) {
+            throw new GetCategoryException("Category with name not found: " + name);
+        }
+    }
+}
---
+++ src/main/java/com/henry/expensetracker/utils/UserUtils.java
@@ -0,0 +1,35 @@
+package com.henry.expensetracker.utils;
+
+import com.henry.expensetracker.entity.User;
+import com.henry.expensetracker.exception.GetUserException;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.henry.expensetracker.repository.impl.UserRepositoryImpl;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserUtils {
+    private final String SQL_GET_USER = "SELECT * FROM users WHERE email = ?";
+
+    @Autowired
+    private UserRepositoryImpl userRepositoryImpl;
+    private JdbcTemplate jdbcTemplate;
+
+    public User getUserByEmail(String email) throws GetUserException {
+        try {
+            return jdbcTemplate.queryForObject(SQL_GET_USER, new Object[]{email}, userRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+            throw new GetUserException("Something was wrong getting user");
+        }
+    }
+
+    RowMapper<User> userRowMapper = (rs, rowNum) -> {
+        return new User(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("email")
+        );
+    };
+}
