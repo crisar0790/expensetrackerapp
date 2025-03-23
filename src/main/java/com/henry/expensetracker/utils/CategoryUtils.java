140754fade8223029d6327bcaef499e71dc1ebe3
crachetoni-at-152410181392 <crachetonireformam@gmail.com>
2025-03-23 18:52:33 -0300

    modify utils classes

--- src/main/java/com/henry/expensetracker/utils/CategoryUtils.java
+++ src/main/java/com/henry/expensetracker/utils/CategoryUtils.java
@@ -1,24 +1,21 @@
 package com.henry.expensetracker.utils;
 
-import com.henry.expensetracker.controller.model.response.CategoryResponse;
+import com.henry.expensetracker.entity.Category;
 import com.henry.expensetracker.exception.GetCategoryException;
+import com.henry.expensetracker.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoryUtils {
-    private final String SQL_GET_CATEGORY_BY_NAME = "SELECT id FROM category WHERE name = ?";
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private CategoryRepository categoryRepository;
 
-    public Long getCategoryId(String name) throws GetCategoryException {
+    public Category getCategoryByName(String name) throws GetCategoryException {
         try {
-            return jdbcTemplate.queryForObject(SQL_GET_CATEGORY_BY_NAME, new Object[]{name}, Long.class);
+            return categoryRepository.findByName(name);
         } catch (EmptyResultDataAccessException e) {
             throw new GetCategoryException("Category with name not found: " + name);
         }
--- src/main/java/com/henry/expensetracker/utils/UserUtils.java
+++ src/main/java/com/henry/expensetracker/utils/UserUtils.java
@@ -2,35 +2,23 @@ package com.henry.expensetracker.utils;
 
 import com.henry.expensetracker.entity.User;
 import com.henry.expensetracker.exception.GetUserException;
+import com.henry.expensetracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.henry.expensetracker.repository.impl.UserRepositoryImpl;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserUtils {
-    private final String SQL_GET_USER = "SELECT * FROM users WHERE email = ?";
 
     @Autowired
-    private UserRepositoryImpl userRepositoryImpl;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private UserRepository userRepository;
 
     public User getUserByEmail(String email) throws GetUserException {
         try {
-            return jdbcTemplate.queryForObject(SQL_GET_USER, new Object[]{email}, userRowMapper);
+            return userRepository.findByEmail(email);
         } catch (EmptyResultDataAccessException e) {
             throw new GetUserException("Something was wrong getting user");
         }
     }
-
-    RowMapper<User> userRowMapper = (rs, rowNum) -> {
-        return new User(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getString("email")
-        );
-    };
 }
