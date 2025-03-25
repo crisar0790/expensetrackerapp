1644ffd39f0ad1b86db5f4f3f5a78211fc2446d5
crachetoni-at-152410181392 <crachetonireformam@gmail.com>
2025-03-25 00:05:04 -0300

    add property into ExpenseResponse class

--- src/main/java/com/henry/expensetracker/controller/model/response/ExpenseResponse.java
+++ src/main/java/com/henry/expensetracker/controller/model/response/ExpenseResponse.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ExpenseResponse {
     private Long id;
+    private String userName;
     private double amount;
     private LocalDate date;
     private String category;
